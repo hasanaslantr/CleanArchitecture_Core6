e3d59cac560c779b882d6143590de5fc84601189
Hasan ASLAN <info@hasanaslan.com>
2023-08-12 23:56:53 +0300

    Creata Folder GenericRepositories adding

---
+++ NLayer.Core/GenericRepositories/IGenericrepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq.Expressions;
+
+namespace NLayer.Core.GenericRepositories
+{
+    public interface IGenericrepository<T> where T : class
+    {
+        IQueryable<T> GetAll();
+        Task<T> GetByIdAsycn(int id);
+        Task<T> GetByIdsAsycn(int id, int id2);
+        IQueryable<T> Where(Expression<Func<T, bool>> filter);
+        Task<bool> AnyAsycn(Expression<Func<T, bool>> filter);
+        Task AddAsycn(T entity);
+        Task AddRangeAsycn(IEnumerable<T> entities);
+        void Update(T entity);
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entites);
+    }
+}
