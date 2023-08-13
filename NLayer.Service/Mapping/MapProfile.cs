5143862e3a58bccc1028ecfac7e0b835844367cd
Hasan ASLAN <info@hasanaslan.com>
2023-08-13 20:17:33 +0300

    Creata NLayer.Service> Exceptions>GenericManger>Mapping    adding

---
+++ NLayer.Service/Mapping/MapProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper; 
+using NLayer.Core.Concrete;
+using NLayer.Core.DTOs.AccountDtos; 
+using static System.Formats.Asn1.AsnWriter;
+using System.Security.Claims;
+
+namespace NLayer.Service.Mapping
+{
+    public class MapProfile : Profile
+    {
+        public MapProfile()
+        {
+            CreateMap<AppUser, AppUserDto>().ReverseMap(); 
+        }
+    }
+}
